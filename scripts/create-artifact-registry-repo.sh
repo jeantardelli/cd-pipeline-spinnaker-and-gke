c4dec90523a511cf5d6d6c94608349a160ed1123
Jean Carlo <jeantardelli@gmail.com>
2023-01-15 14:36:36 +0000

    cloudbuild step 2 fix

--- scripts/create-artifact-registry-repo.sh
+++ scripts/create-artifact-registry-repo.sh
@@ -4,7 +4,7 @@ gcloud config get-value project
 
 # Create the Artifact Registry repo
 gcloud artifacts repositories create ${REPO_NAME} \
-   --project=${PROJECT_ID}
+   --project=${PROJECT_ID} \
    --repository-format=docker \
    --location=${LOCATION} \
    --description="Docker repository"
